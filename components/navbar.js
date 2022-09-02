27142615a3224b9ad2e2ff2ae8d04dc0ada92bd1
Rahul Arepaka <rahul.arepaka@gmail.com>
2022-09-02 10:10:04 +0530

    achievements

--- components/navbar.js
+++ components/navbar.js
@@ -85,10 +85,6 @@ const Navbar = props => {
 
           {/* <LinkItem href="/coursework" path={path}>
             Coursework
-          </LinkItem>
-
-          <LinkItem href="/achievements" path={path}>
-            Achievements
           </LinkItem> */}
         </Stack>
 
@@ -107,6 +103,9 @@ const Navbar = props => {
             </LinkItem>
             <LinkItem href="/projects" path={path}>
               Projects
+            </LinkItem>
+            <LinkItem href="/achievements" path={path}>
+              Achievements
             </LinkItem>
             <LinkItem href="/contact" path={path}>
               Contact
@@ -138,9 +137,9 @@ const Navbar = props => {
                 <NextLink href="/about" passHref>
                   <MenuItem as={Link}>About</MenuItem>
                 </NextLink>
-                {/* <NextLink href="/achievements" passHref>
+                <NextLink href="/achievements" passHref>
                   <MenuItem as={Link}>Achievements</MenuItem>
-                </NextLink> */}
+                </NextLink>
                 <NextLink href="/projects" passHref>
                   <MenuItem as={Link}>Projects</MenuItem>
                 </NextLink>
